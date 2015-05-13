8dd2fc00ed28c5387ce63e1e16a6cfa909d969b1
lateema-vice <lateema.spencer@vice.com>
2015-05-13 16:40:00 -0400

    moved js files to js folder

---
+++ js/validate.js
@@ -0,0 +1,22 @@
+var Person = Backbone.Model.extend({
+	defaults: {
+		name: 'Guest User',
+		age: 23,
+		occupation: 'worker'
+	},
+
+	validate: function(attributes){
+		if(attributes.age < 0){
+			return 'Age must be positive';
+		}
+
+		if(!attributes.name){
+			return 'Every person must have a name.';
+		}
+	},
+
+	work: function(){
+		return this.get('name') + ' is working';
+	}
+
+	});
---
+++ js/views.js
@@ -0,0 +1,24 @@
+var Person = Backbone.Model.extend({
+	defaults: {
+		name: 'Guest User',
+		age: 23,
+		occupation: 'worker'
+	}
+});
+
+var PersonView = Backbone.View.extend(
+{
+	tagName: 'li',
+	// className: 'person',
+	// id: 'person-id'
+	initialize: function(){
+		this.render();
+	},
+
+	render: function(){
+		this.$el.html(this.model.get('name') + 
+			' (' + this.model.get('age') + 
+			') - ' + 
+		this.model.get('occupation'));
+	}
+});
